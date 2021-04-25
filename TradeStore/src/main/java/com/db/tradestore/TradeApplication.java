eb5cdab544fb056713ea62216b2fbe234f0f5093
Kalpana Gagare <kalpana.gagare26@gmail.com>
2021-04-25 23:02:59 +0530

    development : Added Initial Spring Boot set up for Trade Store Application.

---
+++ TradeStore/src/main/java/com/db/tradestore/TradeApplication.java
@@ -0,0 +1,22 @@
+/** Project Trade.*/
+
+package com.db.tradestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot main class.
+ * 
+ * @author kalpana.gagare
+ *
+ */
+@SpringBootApplication
+public class TradeApplication {
+
+	/** Main method. */
+	public static void main(String[] args) {
+		SpringApplication.run(TradeApplication.class, args);
+	}
+
+}
---
+++ TradeStore/src/test/java/com/db/tradestore/TradeApplicationTests.java
@@ -0,0 +1,20 @@
+/** Project Trade.*/
+
+package com.db.tradestore;
+
+/**Dummy Test class to run Initial setUp.
+ * 
+ * */
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TradeApplicationTests {
+
+	/** Dummy Test case. */
+	@Test
+	public void contextLoads() {
+	}
+
+}
