fc2aa60a1abeb67f563bd06180b79fb9933d001e
Kalpana Gagare <kalpana.gagare26@gmail.com>
2021-04-25 23:13:08 +0530

    development : Added constant class.

---
+++ TradeStore/src/main/java/com/db/tradestore/constant/TradeConstant.java
@@ -0,0 +1,13 @@
+/** Project Trade.*/
+
+package com.db.tradestore.constant;
+
+/**
+ * @author SkyWorld Computer
+ *
+ */
+public class TradeConstant {
+
+	/** Constant for Trade Date format. */
+	public static String TRADE_DATE_FORMAT = "dd/MM/yyyy";
+}
